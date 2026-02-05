97e17b869c12a8e70c147ecbbb98c8230583fa2a
Reese Dychiao <rkd9714@nyu.edu>
2026-02-05 17:45:43 +0800

    create wardrobe grid component

--- client/src/components/ItemCard.tsx
+++ client/src/components/ItemCard.tsx
@@ -1,50 +1,12 @@
 import { Card } from "./ui/card";
 import { NavLink } from "react-router-dom";
-import { useState, useEffect } from "react";
 import type { ClothingItem } from "../types";
 
-export default function ItemCard() {
-  const [data, setData] = useState<ClothingItem[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("api_placeholder");
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const result = await response.json();
-        setData(result);
-      } catch (err) {
-        if (err instanceof Error) {
-          setError(err.message);
-        } else {
-          setError(String(err));
-        }
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
+export default function ItemCard(item: ClothingItem) {
   return (
-    <ul>
-      {data &&
-        data.map((item) => (
-          <NavLink key={item.id} to={"/items/" + item.id}>
-            <Card
-              className="
+    <NavLink key={item.id} to={"/items/" + item.id}>
+      <Card
+        className="
                     w-80 
                     aspect-square 
                     flex 
@@ -55,11 +17,9 @@ export default function ItemCard() {
                     hover:shadow-md 
                     transition
                     "
-            >
-              <img src={item.img} alt={item.name} />
-            </Card>
-          </NavLink>
-        ))}
-    </ul>
+      >
+        <img src={item.img} alt={item.name} />
+      </Card>
+    </NavLink>
   );
 }
